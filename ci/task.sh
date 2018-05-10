0f7e37734066e217abfc2223ec101500861e7311
Gavin Edwards <gavin.edwards@ons.gsi.gov.uk>
2018-05-10 14:50:38 +0100

    Remove conditional from task.sh

--- ci/task.sh
+++ ci/task.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ex
 
-if [[ -e ../java-tag ]]; then
-    version_number=$(cat ../java-tag/number)
-    mvn versions:set -DnewVersion=$version_number
-fi
+version_number=$(cat ../java-tag/number)
+mvn versions:set -DnewVersion=$version_number
 
 mvn package
 # Move target for output

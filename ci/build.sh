5e47193ab983ff74da5d96143c62000457c5be1c
Ben Jefferies <benjefferies@echosoft.uk>
2018-09-04 15:23:08 +0100

    Remove debug logging

--- ci/build.sh
+++ ci/build.sh
@@ -2,9 +2,6 @@
 
 mkdir -p .m2/repository
 
-echo "Local repo" .m2/repository
-ls -l .m2/repository
-
 mvn versions:set -DnewVersion=$(git rev-parse HEAD)
 
 mvn package -Dmaven.repo.local=.m2/repository

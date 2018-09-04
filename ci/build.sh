fcc58cad408fd0b558c4e5e10b4e09a0150094d1
Ben Jefferies <benjefferies@echosoft.uk>
2018-09-04 12:14:37 +0100

    Debug what is in cached repo

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 
+echo "Local repo" .m2/repository
+ls -l .m2/repository
+
 mvn versions:set -DnewVersion=$(git rev-parse HEAD)
 
-echo "Local repo" ${ROOT_FOLDER}/.m2/repository
-ls -l ${ROOT_FOLDER}/.m2/repository
-
-mvn package -Dmaven.repo.local=${ROOT_FOLDER}/.m2/repository
+mvn package -Dmaven.repo.local=.m2/repository
 
 cp target/*.jar ../artifacts
